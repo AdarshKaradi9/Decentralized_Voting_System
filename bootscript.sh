b69cd01d2379388c51976c82e0fead11c1c54180
Adarsh Karadi <adarshkaradi009@gmail.com>
2020-03-27 18:09:52 +0530

    commit

--- bootscript.sh
+++ bootscript.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
+cd /var/run
+sudo chmod 777 docker.sock
+cd -
+chmod 777 -R *
 cd ../fabric-samples/first-network
 ./byfn.sh down
-
+docker rm $(docker ps -aq)
 cd ../../Decentralized_Voting_System/Voting
 ./startFabric.sh javascript
-
 cd javascript
 ./remove.sh
-
 node enrollAdmin.js
-
-
-
